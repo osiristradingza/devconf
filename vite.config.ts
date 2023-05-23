e56283a941cd38bb1526b04cdc28cb394180a45a
Gavin Hardaker <ghardaker587@gmail.com>
2023-05-23 15:05:05 +0200

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -21,4 +21,5 @@ export default defineConfig({
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
+  base: process.env.NODE_ENV === "production" ? "/devconf/" : "/",
 });
