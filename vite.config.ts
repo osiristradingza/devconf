cd2aec3e3ea80669021c50be6bed36771af0acee
Gavin Hardaker <ghardaker587@gmail.com>
2023-05-23 14:33:33 +0200

    Put that back

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,9 @@ export default defineConfig({
       },
     }),
   ],
-  // resolve: {
-  //   alias: {
-  //     "@": fileURLToPath(new URL("./src", import.meta.url)),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
